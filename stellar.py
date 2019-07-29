74ea5dc496535d830fd264de114f04e0431660b8
Michael J. Wilson <mjwilson@lbl.gov>
2019-07-29 21:42:41 +0000

    Added SSFR

--- stellar.py
+++ stellar.py
@@ -58,6 +58,8 @@ mean_ssfr     =  np.array([np.mean(ssfr[bsmass == _bin]) for _bin in np.arange(l
 
 pl.plot(np.log10(mean_smass), np.log10(mean_ssfr), c='darkcyan', lw=1)
 
+pl.ylim(-2., 1.)
+
 pl.xlabel(r'$\log_{10}|M_*|$')
 pl.ylabel(r'$\log_{10}|\dot M_* / M_* / \rm{Gyr}^{-1} |$')
 
