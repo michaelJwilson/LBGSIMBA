fe3997432238c46ccf3225ccdce141f088636fa7
Michael James Wilson <MJWilson@MJWilson-m65.local>
2020-01-27 13:40:06 -0800

    ZA running.

--- ZA/run.py
+++ ZA/run.py
@@ -1,9 +1,19 @@
-from real_space_corr_fftw import CorrelationFunction
+import  pylab                as      pl
+import  numpy                as      np
+
+from    real_space_corr_fftw import  CorrelationFunction
 
 
-##  source activate ZA
+# source activate ZA
+
+k, Pk, PNL = np.loadtxt('dat/pklin_z502.txt', unpack=True)
 
 # one_loop =False makes it Zeldovich
-zelda = CorrelationFunction(cosmo.k,cosmo.p, one_loop= False, threads=1, jn=10, shear=True)
+zelda      = CorrelationFunction(k, Pk, one_loop= False, threads=1, jn=10, shear=True)
 
-r, xi = zelda.combine_bias_terms(b1, b2, bs, counterterm)
+# b1, b2, bs, counter-term.
+r, xi      = zelda.combine_bias_terms(0.5, 0.5, 0.5, 0.0)
+
+pl.loglog(r, xi)
+
+pl.show()
