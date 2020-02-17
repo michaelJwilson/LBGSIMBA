70060ae73e88fde01f37c23bec2817390b7d32b2
Michael J. Wilson <mjwilson@lbl.gov>
2020-02-17 18:01:24 +0000

    Init. pz dir.

---
+++ pz/hildebrandt_09.py
@@ -0,0 +1,23 @@
+import  numpy  as np
+import  pylab  as pl
+import  os
+
+
+def getpz_H09():
+  data       = np.loadtxt('dat/udrops_r23p0t25p5_Masim.dat')
+
+  dz         = data[1,0] - data[0,0]
+  norm       = np.sum(data[:,1] * dz)
+
+  pz         = data[:,1] / norm
+  midz       = data[:,0] + 0.5 * dz
+
+  return  midz, pz
+
+
+if __name__ == "__main__":
+  midz, pz   = getpz_H09()
+
+  pl.plot(midz, pz)
+
+  pl.show()
