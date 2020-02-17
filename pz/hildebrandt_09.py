f6eb5ab590bf47534a5ed67b83e66a57b6206edc
Michael James Wilson <MJWilson@MJWilson-m65.local>
2020-02-17 13:20:27 -0500

    Added Hildebrant pzs.

--- pz/hildebrandt_09.py
+++ pz/hildebrandt_09.py
@@ -1,23 +1,38 @@
-import  numpy  as np
-import  pylab  as pl
-import  os
+import numpy as np
+import pylab as pl
+import os
 
 
-def getpz_H09():
-  data       = np.loadtxt('dat/udrops_r23p0t25p5_Masim.dat')
+def getpz_H09(sample='u'):
+  # Samples at supplied zs (every dz = 0.1)
+  if sample == 'u':
+    data  = np.loadtxt('dat/udrops_r23p0t25p5_Masim.dat')
+    
+  elif sample == 'g':
+    data  = np.loadtxt('dat/gdrops_i23p5t26p0_Masim.dat')
 
-  dz         = data[1,0] - data[0,0]
-  norm       = np.sum(data[:,1] * dz)
+  else:
+    raise ValueError('Only u and g drops are aviailable for CARS currently.')
 
-  pz         = data[:,1] / norm
-  midz       = data[:,0] + 0.5 * dz
+  dzs   = np.diff(data[:,0]).astype(np.float32)
+
+  assert  np.all(dzs == dzs[0])
+
+  dz    = data[1,0] - data[0,0]
+  norm  = dz * np.sum(data[:,1])
+
+  pz    = data[:,1] / norm
+  midz  = data[:,0] + 0.5 * dz
+
+  assert  np.isclose(dz * np.sum(pz), 1.00)
 
   return  midz, pz
 
 
 if __name__ == "__main__":
-  midz, pz   = getpz_H09()
+  for sample in ['u', 'g']:
+    midz, pz = getpz_H09(sample=sample)
+    pl.plot(midz, pz, label=r'${}$-dropouts'.format(sample))
 
-  pl.plot(midz, pz)
-
+  pl.legend(frameon=False)
   pl.show()
