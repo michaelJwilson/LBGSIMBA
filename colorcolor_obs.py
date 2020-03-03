2b7fa8d471283bdc946ab01e96863a80c9860e1e
Michael J. Wilson <mjwilson@lbl.gov>
2020-03-03 16:28:12 +0000

    Include tids.

--- colorcolor_obs.py
+++ colorcolor_obs.py
@@ -24,23 +24,23 @@ from    hildebrandt                           import  onesig_mag
 from    mpl_toolkits.axes_grid1.inset_locator import  inset_axes
 
 
-boxsize      =  100.
+boxsize            =  100.
 
-depths       =  get_depths()
+depths             =  get_depths()
 
-nrows        =  -1
-prop         =  'hmass'  ## 'smass'     
+nrows              =  -1
+prop               =  'hmass'  ## 'smass'     
 
-wave, two    =  get_pyloser(boxsize, 2.024621, nrows=nrows)
+wave, two,   ids2  =  get_pyloser(boxsize, 2.024621, nrows=nrows)
 
-wave, three  =  get_pyloser(boxsize, 3.003070, nrows=nrows)
-wave, four   =  get_pyloser(boxsize, 3.963392, nrows=nrows)
-wave, five   =  get_pyloser(boxsize, 5.024400, nrows=nrows)
+wave, three, ids3  =  get_pyloser(boxsize, 3.003070, nrows=nrows)
+wave, four,  ids4  =  get_pyloser(boxsize, 3.963392, nrows=nrows)
+wave, five,  ids5  =  get_pyloser(boxsize, 5.024400, nrows=nrows)
 
-phys2        =     get_phys(boxsize, 2.024621, nrows=nrows)['hmass']
-phys3        =     get_phys(boxsize, 3.003070, nrows=nrows)['hmass']
-phys4        =     get_phys(boxsize, 3.963392, nrows=nrows)['hmass']
-phys5        =     get_phys(boxsize, 5.024400, nrows=nrows)['hmass']
+phys2              =  get_phys(boxsize, 2.024621, nrows=nrows)['hmass']
+phys3              =  get_phys(boxsize, 3.003070, nrows=nrows)['hmass']
+phys4              =  get_phys(boxsize, 3.963392, nrows=nrows)['hmass']
+phys5              =  get_phys(boxsize, 5.024400, nrows=nrows)['hmass']
 
 ##  Available redshifts: [3.00307, 2.024621, 3.963392, 5.0244]                                                                                                                                                                        
 ##  Available snapshots: ['062',   '078',    '051',    '042']
